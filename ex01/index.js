de13a2c7cdf11d07ce15ddcc72a09248377c0388
Tales Gomes <tales.santos.gomes@outlook.com>
2025-04-06 19:25:34 -0300

    Exercicio 01 finalizado

---
+++ ex01/index.js
@@ -0,0 +1,13 @@
+console.log("Desafio 1");
+console.log("Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0 e imprimir a soma");
+
+let indice = 13;
+let soma = 0;
+let k = 0;
+
+while (k < indice) {
+  k = k + 1;
+  soma += k;
+}
+
+console.log(soma); // Imprime o valor final de SOMA
